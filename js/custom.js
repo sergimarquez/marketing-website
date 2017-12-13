f5a34a379d245391dc1f1c9253f7517176c18d0d
Sergi Marquez <sertrack8@gmail.com>
2017-12-13 16:14:54 +0100

    magnific popup

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,16 @@
 $(function() {
 	// activte on scroll
 	new WOW().init();
+}); 
+
+// WORK
+
+$(function() {
+  $("#work").magnificPopup({
+  	delegate: 'a', // child items selector, by clicking on it popup will open
+  	type: 'image',
+  	gallery: {
+    	enabled:true
+  	}
+  });
 });
