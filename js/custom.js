1d96ce277ac22f7f45a9e13a03f7dc1393e1fba4
Sergi Marquez <sertrack8@gmail.com>
2017-12-14 16:41:18 +0100

    testimonials owl carousel

--- js/custom.js
+++ js/custom.js
@@ -32,3 +32,17 @@ $(function(){
   });
 
 });
+
+// TESTIMONIALS
+
+$(function(){
+
+  $("#customers-testimonials").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true
+  });
+
+});
