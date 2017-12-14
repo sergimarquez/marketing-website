b7fa71f3e7bfb0bee59c500aa0a9ae274d6fbe60
Sergi Marquez <sertrack8@gmail.com>
2017-12-14 12:45:49 +0100

    team section crousel slider

--- js/custom.js
+++ js/custom.js
@@ -8,11 +8,26 @@ $(function() {
 // WORK
 
 $(function() {
+
   $("#work").magnificPopup({
   	delegate: 'a', // child items selector, by clicking on it popup will open
   	type: 'image',
   	gallery: {
     	enabled:true
   	}
+
   });
+});
+
+// TEAM
+
+$(function(){
+
+  $("#team-members").owlCarousel({
+    items: 3,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true
+  });
+
 });
