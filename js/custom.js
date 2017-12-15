55cce9ec727e011d3055eaae67c92ea38f485c87
Sergi Marquez <sertrack8@gmail.com>
2017-12-15 17:24:26 +0100

    clients carousel

--- js/custom.js
+++ js/custom.js
@@ -47,6 +47,28 @@ $(function(){
 
 });
 
+// CLIENTS
+
+$(function(){
+
+  $("#clients-list").owlCarousel({
+    items: 6,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true
+  });
+
+});
+
+
+
+
+
+
+
+
+
 // STATS
 
 $(function(){
