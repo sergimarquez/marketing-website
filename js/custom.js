993e636e6e3a7a1be4af60a738717a5c53350878
Sergi Marquez <sertrack8@gmail.com>
2017-12-17 20:51:22 +0100

    medium devices responsive

--- js/custom.js
+++ js/custom.js
@@ -114,6 +114,15 @@ $(function(){
   });
 });
 
+// Close mobile menu on click
+
+$(function(){
+
+  $(".navbar-collapse ul li a").on("click touch", function(){
+
+    $(".navbar-toggle").click();
+  });
+});
 
 
 
